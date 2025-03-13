f6e0d1434feb5c7b3619eefbf5041be492d38ba1
khalid <k.atiab.idrissi@gmail.com>
2025-03-13 12:45:43 +0000

    category model,like comment serializer

--- apps/stories/models.py
+++ apps/stories/models.py
@@ -4,13 +4,29 @@ from apps.accounts.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # Tracks last modification time
+
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural= "Categories"
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Story(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
+    Category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True) 
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ImageField(upload_to="media/",null=True,blank=True)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # Tracks last modification time
+
 
     class Meta:
         verbose_name = 'Story'
--- apps/stories/serializerz.py
+++ apps/stories/serializerz.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from .models import Story
+from .models import Story, Like, Comment
 
 class StorySerializer (serializers.ModelSerializer):
     class Meta:
         model = Story
         fields = ("id", "title", "content", "author", "image")
         read_only_field = ("id")
+
+
+class LikeSerializer(serializers.ModelSerializer):
+
+    user = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'story', 'created_at'] 
+        read_only_fields = ['id', 'user', 'created_at']  
+ 
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    user = serializers.ReadOnlyField(source="user.username")
+    story = serializers.PrimaryKeyRelatedField(read_only=True)  
+
+    class Meta:
+        model = Comment
+        fields = ["id", "title", "content", "created_at"]
+        read_only_fields = ['id', 'user', 'story', 'created_at']  # Auto-filled fields
+
+
+
+
+        
