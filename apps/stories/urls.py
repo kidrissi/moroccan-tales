c3722cc005f21edc86fbc0eae1309a873f8a3321
khalid <k.atiab.idrissi@gmail.com>
2025-03-15 14:18:06 +0000

    allow front end to fetch api

--- apps/stories/urls.py
+++ apps/stories/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import StoryGetView, StoryPostView, StoryUpdateView, StoryDeleteView, CategoryViewSet,LikeView
+from .views import StoryGetView, StoryPostView, StoryUpdateView, StoryDeleteView, CategoryViewSet,LikeView, CommentView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -13,7 +13,7 @@ urlpatterns = [
     path('story/delete/<int:story_id>/', StoryDeleteView.as_view(), name='story-delete'),
     path("categories/",include(router.urls)),
     path('story/<int:story_id>/like',LikeView.as_view(), name="like-story"),
-    path('story/<int:story_id>/comment',commentView.as_view(), name="comment-story")
+    path('story/<int:story_id>/comment',CommentView.as_view(), name="comment-story")
 
 
 
--- moroccan_tales/settings.py
+++ moroccan_tales/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'moroccan_tales.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  
+    ]
 
 TEMPLATES = [
     {
